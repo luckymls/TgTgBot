a92b8ccd898dae21347295fd56ff72e529d05bb0
luckymls <melis.luca2014@gmail.com>
2022-03-18 14:46:16 +0100

    General interface2

---
+++ main.py
@@ -0,0 +1,15 @@
+#TODO import Client (library)
+
+
+email = ""
+client = Client(email)
+access_data = client.get_credentials()
+
+access_token = access_data['access_token']
+refresh_token =  access_data['refresh_token']
+user_id = access_data['user_id']
+
+client = Client(access_token, refresh_token, user_id)
+
+items_faved = client.get_items()
+
